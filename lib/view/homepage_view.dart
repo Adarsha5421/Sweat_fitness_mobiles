ac74850ac84daa2dda9c7ee0d4e03043709fb088
adarsa <asdf@asdf.com>
2025-03-05 16:12:53 +0545

    title

--- lib/view/homepage_view.dart
+++ lib/view/homepage_view.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sweat"),
-        backgroundColor: const Color.fromARGB(290, 290, 56, 75),
+        backgroundColor: const Color.fromARGB(290, 290, 56, 15),
       ),
       body: const Padding(
         padding: EdgeInsets.all(16.0),
